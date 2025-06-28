506d8707c5f2f433cbb5974a7a9205b459a4b2cd
Oleksandr Malakhov <71847145+hisio13@users.noreply.github.com>
2025-06-28 18:01:53 +0200

    add index file

--- src/controllers/contactsController.js
+++ src/controllers/contactsController.js
@@ -1,4 +1,13 @@
-import { getContactById } from '../services/contacts.js';
+import { getAllContacts, getContactById } from '../services/contacts.js';
+
+export const getContactsController = async (req, res) => {
+  const contacts = await getAllContacts();
+  res.status(200).json({
+    status: 200,
+    message: 'Successfully found contacts!',
+    data: contacts,
+  });
+};
 
 export const getContactByIdController = async (req, res) => {
   const { contactId } = req.params;
