e9af7f0e135dfcf2e0aa8de312979a968c70d3af
Oleksandr Malakhov <71847145+hisio13@users.noreply.github.com>
2025-06-28 18:16:20 +0200

    add index file

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -9,8 +9,7 @@ export async function initMongoConnection() {
       MONGODB_DB,
     } = process.env;
 
-    const encodedPassword = encodeURIComponent(MONGODB_PASSWORD);
-    const uri = `mongodb+srv://${MONGODB_USER}:${encodedPassword}@${MONGODB_URL}/${MONGODB_DB}?retryWrites=true&w=majority`;
+    const uri = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_URL}/${MONGODB_DB}?retryWrites=true&w=majority`;
 
     await mongoose.connect(uri);
     console.log('✅ Mongo connection successfully established!');
