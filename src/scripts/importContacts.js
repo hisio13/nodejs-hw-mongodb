9074ab9601d7709a7aa3093a51fad3b0624d5dba
Oleksandr Malakhov <71847145+hisio13@users.noreply.github.com>
2025-07-06 22:49:55 +0200

    all

--- src/scripts/importContacts.js
+++ src/scripts/importContacts.js
@@ -2,7 +2,7 @@ import fs from 'fs/promises';
 import path from 'path';
 import dotenv from 'dotenv';
 import { initMongoConnection } from '../db/initMongoConnection.js';
-import { ContactsCollection } from '../models/contact.js';
+import { ContactsCollection } from '../models/contactModel.js';
 
 dotenv.config();
 
