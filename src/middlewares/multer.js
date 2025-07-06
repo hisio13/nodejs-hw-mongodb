aaefc5b2fb57e513caee61fbf128f12cf01a3c84
Oleksandr Malakhov <71847145+hisio13@users.noreply.github.com>
2025-07-06 22:59:54 +0200

    all

--- src/middlewares/multer.js
+++ src/middlewares/multer.js
@@ -1,4 +1,20 @@
 import multer from 'multer';
-import { storage } from '../services/cloudinary.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const tempDir = path.join(__dirname, '../../tmp');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, tempDir);
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    cb(null, `${uniqueSuffix}-${file.originalname}`);
+  },
+});
 
 export const upload = multer({ storage });
